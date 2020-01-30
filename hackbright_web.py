d1eaa9b003ef97e5eeb33a3486a58e75cf7f539b
Your Name <you@example.com>
2020-01-30 12:55:39 -0800

    added student's info to project information page

--- hackbright_web.py
+++ hackbright_web.py
@@ -41,10 +41,14 @@ def project_info():
 
     title, description, max_grade = hackbright.get_project_by_title(title)
 
+    grades = hackbright.get_grades_by_title(title)
 
-    project_title_html = render_template('project.html', title=title,
+
+    project_title_html = render_template('project.html', 
+                                        title=title,
                                         description=description,
-                                        max_grade=max_grade)
+                                        max_grade=max_grade,
+                                        grades=grades)
 
     return project_title_html
 
